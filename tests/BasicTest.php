540bfb49a48bf76adf8db85d67a78b6deca20413
Lewis Cowles <lewiscowles@me.com>
2017-07-09 11:10:15 +0100

    Tests
    Initial unit-tests, mostly to check current design goals. Initially I just wanted [the original](https://github.com/imiskolee/FormUpload/issues/1) to not be bound to CURL, but it evolved when I wasn't quite sure what was going on, and it turned out there were bugs requiring a structural overhaul.
    
    ## Goals
    * To pass examples (case-sensetivity and typo's in original examples aside)
    * To work without too much coupling (failed and had to use Guzzle to overcome)
    * To provide a workable replacement to the original library

---
+++ tests/BasicTest.php
@@ -0,0 +1,107 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use GuzzleHttp\Psr7\Request;
+
+use lewiscowles\Rfc\Envelope;
+use lewiscowles\Rfc\FormBody;
+use lewiscowles\Rfc\NodeInterface;
+
+class BasicTest extends TestCase
+{
+    const JOE_BLOW_TEXT = 'content-disposition: form-data; name="submitter"'."\n\n".'Joe Blow';
+    const FILE1_TEXT_FLAT = 'content-disposition: form-data; name="file1"; filename="file1.txt"'."\n".'content-type: text/plain'."\n\n".
+                            '...contents of file1.txt...';
+    const FILE1_TEXT_MULTI = 'content-disposition: attachment; filename="file1.txt"'."\n".'content-type: text/plain'."\n\n".
+                       '...contents of file1.txt...';
+    const FILE2_TEXT_MULTI = 'content-disposition: attachment; filename="file2.gif"'."\n".'content-type: image/gif'."\n".
+                       'content-transfer-encoding: binary'."\n\n".'...contents of file2.gif...';
+    const ENVELOPE_MULTI = 'content-disposition: form-data; name="pics"'."\n".'content-type: multipart/mixed, boundary=';
+    
+    public function setup()
+    {
+        $this->body = new FormBody(new Envelope('ROOT'));
+    }
+    
+    /**
+     * @test
+     */
+    public function it_accepts_any_valid_psr7_requestinterface_implementation() {
+        $request = new Request('GET', 'https://www.github.com');
+        $out = $this->body->submit($request);
+        $this->assertTrue($out->getMethod() == FormBody::HTTP_METHOD);
+    }
+    
+    /**
+     * @test
+     */
+    public function it_does_not_mutate_requests() {
+        $request = new Request('GET', 'https://www.github.com');
+        $out = $this->body->submit($request);
+        $this->assertTrue($request->getMethod() != $out->getMethod());
+    }
+    
+    /**
+     * @test
+     */
+    public function it_accepts_a_form_input() {
+        $this->body->addFormInput('number', $this->getValue('number'));
+    }
+    
+    /**
+     * @test
+     */
+    public function it_accepts_many_parts() {
+        $this->body->addFormInput('number', $this->getValue('number'));
+        $this->body->addFormInput('float', $this->getValue('float'));
+        $this->body->addFormInput('string', $this->getValue('string'));
+    }
+    
+    /**
+     * @test
+     */
+    public function it_produces_the_output_described_in_rfc_example1() {
+        $this->body->addFormInput('submitter', 'Joe Blow');
+        $this->body->addAttachment('file1', '...contents of file1.txt...', 'text/plain', 'file1.txt');
+        $body = "{$this->body}";
+        
+        $this->assertContains(self::JOE_BLOW_TEXT, $body);
+        $this->assertContains(self::JOE_BLOW_TEXT, $body);
+        $this->assertContains(self::FILE1_TEXT_FLAT, $body);
+    }
+    
+    /**
+     * @test
+     */
+    public function it_produces_the_output_described_in_rfc_example2() {
+        $this->body->addFormInput('submitter', 'Joe Blow');
+        
+        // note when adding multiple attachments, the value changes after the first is added.
+        $this->body->addAttachment('pics', '...contents of file1.txt...', 'text/plain', 'file1.txt');
+        $this->body->addAttachment('pics', '...contents of file2.gif...', 'image/gif', 'file2.gif');
+        
+        $body = "{$this->body}";
+        
+        $this->assertContains(self::JOE_BLOW_TEXT, $body);
+        $this->assertContains(self::ENVELOPE_MULTI, $body);
+        $this->assertContains(self::FILE1_TEXT_MULTI, $body);
+        $this->assertContains(self::FILE2_TEXT_MULTI, $body);
+        //file_put_contents(__DIR__ . '/../tmp/out', $body);
+    }
+    
+    protected function getValue($type) {
+        switch(strtolower($type)) {
+            case 'number':
+                return rand(PHP_INT_MIN, PHP_INT_MAX);
+            case 'float':
+                return lcg_value()*(float)(rand(0,99));
+            case 'string':
+                return 'string';
+            case 'file':
+                return 'filefilefilefile';
+        }
+        return true;
+    }
+}
+
